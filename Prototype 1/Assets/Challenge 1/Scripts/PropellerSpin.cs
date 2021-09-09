8a9d0e83360faa44d8783a916ad8d6174e491b87
MessyAttorney16 <ljhalstead16@gmail.com>
2021-09-09 11:01:50 -0500

    plane commit

---
+++ Prototype 1/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    public GameObject propeller;
+    public Vector3 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * speed);
+    }
+}
--- Prototype 1/Assets/PlayerController.cs
+++ Prototype 1/Assets/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
